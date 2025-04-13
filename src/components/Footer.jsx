44c1329af4791a1388711b28464f97004a69d62d
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-04-13 12:17:08 +0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../assets/css/footer.css'
+import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footer-section'>
@@ -21,17 +22,38 @@ const Footer = () => {
             </div>
           </div>
           <div className="footer-box2">
-            2
+            <div className='box2-title'>
+              <h4>Kurslar</h4>
+            </div>
+            <div className="box-ul">
+              <ul>
+                <li>Asosiy</li>
+                <li>Kurslae</li>
+                <li>Mentorlar</li>
+                <li>Biz Haqimizda</li>
+                <li>Aloga</li>
+              </ul>
+            </div>
           </div>
           <div className="footer-box3">
-            3
+            
           </div>
           <div className="footer-box4">
-            4
+          <h3>Bog'lanish</h3>
+    <p><i class="fa-solid fa-location-dot" style={{color: "#ff0000",fontSize:"22px"}}></i> Toshkent, Amir Temur shoh ko’chasi, 100A.</p>
+    <p><i class="fa-solid fa-phone" style={{color: "#ff0000",fontSize:"22px"}}></i> +998 (94) 782 00 92</p>
+    <p><i class="fa-solid fa-envelope" style={{color: "#ff0000",fontSize:"22px"}}></i> ittimeacademy@gmail.com</p>
+    <p><i class="fa-solid fa-clock" style={{color: "#ff0000",fontSize:"22px"}}></i>Du-Sha: 09:00 - 22:00</p>
           </div>
         </div>
         <div className='footer-description'>
-
+          <p>2025IT Time AcademyBarcha huquqlar himoyalangan</p>
+          <div className='fppter-des-flex'>
+          <p>Maxfiylik siyosati
+          </p>
+          <p>Foydalanish shartlari
+          </p>
+          </div>
         </div>
       </div>
     </div>
