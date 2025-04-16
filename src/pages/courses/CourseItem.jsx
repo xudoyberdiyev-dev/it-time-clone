8c51b60029de00761e8b70193e80fd80f0d244b0
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-04-16 15:19:22 +0500

    full course

--- src/pages/courses/CourseItem.jsx
+++ src/pages/courses/CourseItem.jsx
@@ -1,59 +1,65 @@
 import React, { useState } from 'react'
 import '../../assets/css/courseItem.css'
+import { useParams } from 'react-router-dom'
+import { courses } from '../../utils/course'
 const CourseItem = () => {
     const [isModal,setIsModal]=useState(false)
-
+    const {id}=useParams()
+    const course =courses.find(course=>course.id===id)
     const toggleModal=()=>{
         setIsModal(!isModal)
     }
+
+
+
   return (
     <div className='item-container'>
         <div className="item-body">
             <div className="item-box1">
                 <div className='item-img'>
-                    <img src="" alt="" />
+                    <img src={course.img} alt="" />
                 </div>
                 <div className='item-img-body'>
                     <div className='item-title'>
                         <h2>Kurs haqida</h2>
-                        <p>Server tomonini ishlab chiqish. Node.js, Python, ma'lumotlar bazasi va API yaratish.</p>
+                        <p>{course.about}</p>
                     </div>
                     <div className='item-about'>
                         <h2>Davomiligi</h2>
-                        <p>6 oy</p>
+                        <p>{course.muddat}</p>
                     </div>
                     <button className='item-button' onClick={toggleModal}>salom mollar</button>
                 </div>
                 
             </div>
             <div className="item-box2">
-                <h2>BAck-end</h2>
+                <h2>{course.name}</h2>
                 <div className='price-box'>
                 <div className='pr'>
-                    <h2>Kurs tuliq narxi</h2>
+                    <h2>Kursning to'liq narxi</h2>
                         <div className='pr-moey'>
-                            12000000
+                            {course.price}
                         </div>
-                        <p>sasassasa</p>
+                        <p>Oylik to'lov</p>
                         <div style={{width:"100%",height:"1px",backgroundColor:"red",margin:"16px 0 0"}}/>
                         <div className='pr-moeyy'>
-                        <h2>Jami tulov</h2>
-                            12000000
+                        <h2>Jami to'lov</h2>
+                            {course.totalPrice}
                         </div>
                     </div>
                 </div>
                 <div className='credit-price-box'>
                 <div className='cpr'>
-                    <h2>Kurs tuliq narxi</h2>
+                    <h2>Bo'lib to'lash shartlari</h2>
                         <div className='cpr-moey'>
-                            12000000
+                            {course.creditPrice}
                         </div>
-                        <p>sasassasa</p>
+                        <p>Oylik to'lov</p>
                         <div style={{width:"100%",height:"1px",backgroundColor:"red",margin:"16px 0 0"}}/>
                         <div className='cpr-moeyy'>
-                        <h2>Jami tulov</h2>
-                            12000000
-                            <p>Salom salom</p>
+                        <h2>Bo'lib to'lash bilan jami</h2>
+                            {course.creditAllPrice}
+                            <p>Bo'lib to'lash haqida muhim ma'lumot</p>
                         </div>
                     </div> 
                 </div>
